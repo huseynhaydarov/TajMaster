ff44582a6fd6c8405f472fd59dae7409d0b9dfae
huseyn <huseynjonhaydarov@gmail.com>
2025-01-07 19:36:32 +0500

    added get by ids async method

--- TajMaster.Application/Common/Interfaces/Repositories/IRepository.cs
+++ TajMaster.Application/Common/Interfaces/Repositories/IRepository.cs
@@ -6,7 +6,7 @@ public interface IRepository<T> where T : class
 {
     Task<List<T>> GetAllAsync(PagingParameters pagingParameters, CancellationToken cancellationToken = default);
     Task<T?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
-    Task<IList<T>> GetGetByIdsAsync(IEnumerable<int> ids, CancellationToken cancellationToken = default);
+    Task<IList<T>> GetByIdsAsync(IEnumerable<int> ids, CancellationToken cancellationToken = default);
     Task<T> CreateAsync(T entity, CancellationToken cancellationToken = default);
     Task UpdateAsync(T entity, CancellationToken cancellationToken = default);
     Task DeleteAsync(T entity, CancellationToken cancellationToken = default);
