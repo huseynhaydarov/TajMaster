d46a4945f9122bbcbf8ab63cf6a076bcd6a75a0c
huseyn <huseynjonhaydarov@gmail.com>
2025-01-07 19:13:12 +0500

    fixed automapper configuration

--- TajMaster.Application/Mappers/AutoMapperConfiguration.cs
+++ TajMaster.Application/Mappers/AutoMapperConfiguration.cs
@@ -1,17 +1,22 @@
 using AutoMapper;
+ausing TajMaster.Application.UseCases.Cart.CartDtos;
 using TajMaster.Application.UseCases.Cart.Commands;
+using TajMaster.Application.UseCases.CartItem.CartItemDTos;
+using TajMaster.Application.UseCases.Categories.CategoryDto;
 using TajMaster.Application.UseCases.Categories.Commands.Create;
 using TajMaster.Application.UseCases.Categories.Commands.Update;
 using TajMaster.Application.UseCases.Craftsmen.Commands.Create;
-using TajMaster.Application.UseCases.DTO;
-using TajMaster.Application.UseCases.DTOs;
+using TajMaster.Application.UseCases.Craftsmen.Commands.Update.UpdateCraftsman;
+using TajMaster.Application.UseCases.Craftsmen.CraftsmanDTos;
 using TajMaster.Application.UseCases.OrderItems;
 using TajMaster.Application.UseCases.Orders.Create;
 using TajMaster.Application.UseCases.Orders.OrderDtos;
 using TajMaster.Application.UseCases.Reviews.Commands.Create;
 using TajMaster.Application.UseCases.Reviews.Commands.Update;
+using TajMaster.Application.UseCases.Reviews.ReviewDtos;
 using TajMaster.Application.UseCases.Services.Commands.Create;
 using TajMaster.Application.UseCases.Services.Commands.Update;
+using TajMaster.Application.UseCases.Services.ServiceDtos;
 using TajMaster.Application.UseCases.Users.Commands.Create;
 using TajMaster.Application.UseCases.Users.Commands.Update;
 using TajMaster.Application.UseCases.Users.UserDtos;
@@ -25,16 +30,14 @@ public class AutoMapperConfiguration : Profile
     {
         CreateMap<CreateUserCommand, User>();
         CreateMap<UpdateUserCommand, User>();
-       
+
         CreateMap<CreateServiceCommand, Service>()
             .ForMember(dest => dest.Categories, opt => opt.Ignore());
-        CreateMap<UpdateServiceCommand, Service>();
-        CreateMap<Service, ServiceDto>()
-            .ForMember(dest => dest.ServiceId, opt => opt.MapFrom(src => src.Id))
-            .ForMember(dest => dest.Categories, opt => opt.MapFrom(src => src.Categories));
+        CreateMap<UpdateServiceCommand, Service>()
+            .ForMember(dest => dest.Categories, opt => opt.Ignore());
 
-       CreateMap<CreateReviewCommand, Review>();
-       CreateMap<UpdateReviewCommand, Review>();
+        CreateMap<CreateReviewCommand, Review>();
+        CreateMap<UpdateReviewCommand, Review>();
         CreateMap<Review, ReviewDto>()
             .ForMember(dest => dest.ReviewId, opt => opt.MapFrom(src => src.Id))
             .ForMember(dest => dest.CreatedDate, opt => opt.MapFrom(src => src.ReviewDate));
@@ -42,30 +45,20 @@ public class AutoMapperConfiguration : Profile
         CreateMap<OrderItem, OrderItemDto>();
 
         CreateMap<CreateOrderCommand, Order>();
-        /*CreateMap<Order, OrderDto>()
-            .ForMember(dest => dest.OrderId, opt => opt.MapFrom(src => src.Id))
-            .ForMember(dest => dest.OrderStatus, opt => opt.MapFrom(src => src.Status.ToString()));*/
 
-        CreateMap<CreateCraftsmanCommand, Craftsman>();
-        CreateMap<Craftsman, CraftsmanDto>()
-            .ForMember(dest => dest.CraftsmanId, opt => opt.MapFrom(src => src.Id))
-            .ForMember(dest => dest.Specialization, opt => opt.MapFrom(src => src.Specialization.ToString()))
-            .ForMember(dest => dest.Avialable, opt => opt.MapFrom(src => src.IsAvialable));
+        
+        CreateMap<CreateCraftsmanCommand, Craftsman>()
+            .ForMember(c => c.Description, opt => opt.MapFrom(src => src.About))
+            .ForMember(c => c.Specialization , opt => opt.MapFrom(src => src.Specialization))
+            .ForMember(c => c.ProfilePicture, opt => opt.Ignore());
+        CreateMap<UpdateCraftsmanCommand, Craftsman>();
 
         CreateMap<CreateCategoryCommand, Category>();
         CreateMap<UpdateCategoryCommand, Category>();
-        CreateMap<Category, CategoryDto>()
-            .ForMember(dest => dest.CategoryId, opt => opt.MapFrom(src => src.Id))
-            .ForMember(dest => dest.Services, opt => opt.MapFrom(src => src.Services));
-
+     
         CreateMap<CartItem, CartItemDto>()
-            .ForMember(dest => dest.Total, opt => opt.MapFrom(src => src.Price * src.Quantity))
             .ForMember(dest => dest.ServiceName, opt => opt.MapFrom(src => src.Service.Title));
 
         CreateMap<CreateCartCommand, Cart>();
-        CreateMap<Cart, CartDto>()
-            .ForMember(dest => dest.CartId, opt => opt.MapFrom(src => src.Id))
-            .ForMember(dest => dest.CartStatus, opt => opt.MapFrom(src => src.CartStatus.ToString()))
-            .ForMember(dest => dest.CartItems, opt => opt.MapFrom(src => src.CartItems));
     }
 }
