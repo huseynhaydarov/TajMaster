be743697546b5a3e717347b007bbe60da4acb557
huseyn <huseynjonhaydarov@gmail.com>
2025-01-08 14:03:54 +0500

    added FixUserPasswordLength migration

--- TajMaster.Infrastructure/Migrations/20250108085905_FixUserPasswordLength.Designer.cs
+++ TajMaster.Infrastructure/Migrations/20250108085905_FixUserPasswordLength.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TajMaster.Infrastructure.Persistence.Data;
@@ -11,9 +12,11 @@ using TajMaster.Infrastructure.Persistence.Data;
 namespace TajMaster.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250108085905_FixUserPasswordLength")]
+    partial class FixUserPasswordLength
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TajMaster.Infrastructure/Migrations/20250108085905_FixUserPasswordLength.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TajMaster.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixUserPasswordLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "hashedpassword",
+                table: "Users",
+                type: "character varying(256)",
+                maxLength: 256,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "hashedpassword",
+                table: "Users",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(256)",
+                oldMaxLength: 256);
+        }
+    }
+}
