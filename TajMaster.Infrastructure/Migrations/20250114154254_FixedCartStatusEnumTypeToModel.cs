ab29de73ea769e92f8eca3e65e525f57f6232191
huseyn <huseynjonhaydarov@gmail.com>
2025-01-17 10:40:51 +0500

    added migrations

---
+++ TajMaster.Infrastructure/Migrations/20250114154254_FixedCartStatusEnumTypeToModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TajMaster.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixedCartStatusEnumTypeToModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Carts_CartStatuses_Id",
+                table: "Carts");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "CartStatusId",
+                table: "CartStatuses",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartStatuses_Carts_Id",
+                table: "CartStatuses",
+                column: "Id",
+                principalTable: "Carts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartStatuses_Carts_Id",
+                table: "CartStatuses");
+
+            migrationBuilder.DropColumn(
+                name: "CartStatusId",
+                table: "CartStatuses");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Carts_CartStatuses_Id",
+                table: "Carts",
+                column: "Id",
+                principalTable: "CartStatuses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
---
+++ TajMaster.Infrastructure/Migrations/20250116145029_FixedCartStatusRelationWithCart.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TajMaster.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixedCartStatusRelationWithCart : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CartStatus",
+                table: "Carts");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "CartStatusId",
+                table: "Carts",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Carts_CartStatusId",
+                table: "Carts",
+                column: "CartStatusId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Carts_CartStatuses_CartStatusId",
+                table: "Carts",
+                column: "CartStatusId",
+                principalTable: "CartStatuses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Carts_CartStatuses_CartStatusId",
+                table: "Carts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Carts_CartStatusId",
+                table: "Carts");
+
+            migrationBuilder.DropColumn(
+                name: "CartStatusId",
+                table: "Carts");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CartStatus",
+                table: "Carts",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
