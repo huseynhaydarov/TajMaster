2ba8fc9e5f410622f6cc96cb79cdf620ba2b1e2c
huseyn <huseynjonhaydarov@gmail.com>
2025-01-21 20:49:52 +0500

    removed redundant code

--- TajMaster.Application/Common/Behaviors/LoggingBehavior.cs
+++ TajMaster.Application/Common/Behaviors/LoggingBehavior.cs
@@ -6,7 +6,7 @@ namespace TajMaster.Application.Common.Behaviors;
 
 public class LoggingBehavior<TRequest, TResponse>(ILogger<LoggingBehavior<TRequest, TResponse>> logger)
     : IPipelineBehavior<TRequest, TResponse>
-    where TRequest : notnull, IRequest<TResponse>
+    where TRequest : IRequest<TResponse>
     where TResponse : notnull
 {
     public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next,
